9dad413aceb47e094515a67137d8f4ca9b6879a2
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2026-02-05 01:07:40 +0100

    DB init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: configService.get<string>('DB_DEV_PASSWORD'),
+        database: 'tracknotes',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
