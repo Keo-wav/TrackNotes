ae759e581a9cef81548c5d2915d32ca71325c5c2
keo <n.nackphouminh@gmail.com>
2026-02-11 16:39:06 +0100

    FIX - backend link

--- frontend/src/environments/api-urls.ts
+++ frontend/src/environments/api-urls.ts
@@ -1,8 +1,6 @@
-const BASE_URL = 'http://localhost:3000';
-
 export const apiUrls = {
-  users: `${BASE_URL}/users`,
-  projects: `${BASE_URL}/projects`,
-  tracks: `${BASE_URL}/tracks`,
-  comments: `${BASE_URL}/comments`,
+  users: `/api/users`,
+  projects: `/api/projects`,
+  tracks: `/api/tracks`,
+  comments: `/api/comments`,
 }
