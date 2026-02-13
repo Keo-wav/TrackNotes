1b95773a063f4a2401f7e144db5c268d159de554
keo-wav <n.nackphouminh@gmail.com>
2026-02-13 21:55:04 +0100

    FEAT - navlink switch whether on dashboard or project page

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component, inject} from '@angular/core';
+import { Component, computed, inject} from '@angular/core';
 import { ModalService } from '../../services/modal/modal.service';
-import {RouterLink} from '@angular/router';
+import {NavigationEnd, Router, RouterLink} from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {filter, map} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [
     RouterLink
   ],
@@ -12,8 +15,20 @@ import {RouterLink} from '@angular/router';
 })
 export class NavbarComponent {
   modalService = inject(ModalService);
+  private router = inject(Router);
 
   onAddNewProject() {
     this.modalService.open('createProject');
   }
+
+  private url = toSignal(
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => (event as NavigationEnd).urlAfterRedirects)
+    ),
+    { initialValue: this.router.url }
+  );
+
+  isDashboard = computed(() => this.url().includes('/dashboard') || this.url() === '/');
+  isProjectPage = computed(() => this.url().includes('/projects/'));
 }
